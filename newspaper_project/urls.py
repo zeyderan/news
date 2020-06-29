8e193bf407001d7c6fdf0afe966ee6b0c132f2cf
zeyderan <aykutbayraktar34@gmail.com>
2020-06-29 14:02:51 +0300

    vs code geçildi

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path('',include('pages.urls'))
+    path('',include('pages.urls')) # pages app url config eklendi
 ]
